df6ea78bec0fa97eff4ad97c78d6910f44b73f80
Yu-da-1 <yudai.ishi.0704@gmail.com>
2025-03-16 18:27:12 +0900

    generate key

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,5 @@
 use sha2::{Sha256, Digest};
-
+use k256::{ecdsa::{SigningKey, VerifyingKey}, elliptic_curve::rand_core::OsRng};
 // // Peerを表す構造体
 // #[derive(Debug, Clone)]
 // struct Peer {
@@ -22,13 +22,14 @@ fn xor_distance(peer_id: &[u8; 32], content_id: &[u8; 32]) -> [u8; 32] {
     }
     dist
 }
+
 // XOR距離の大小を比較
 fn compare_xor_distance(d1: &[u8; 32], d2: &[u8; 32]) -> std::cmp::Ordering {
     d1.cmp(d2)
 }
 
 // peer_idの生成
-fn generate_peer_id(public_key: &str) -> [u8; 32] {
+fn generate_peer_id(public_key: &[u8]) -> [u8; 32] {
     let mut hasher = Sha256::new();
     hasher.update(public_key);
     let result = hasher.finalize();
@@ -49,14 +50,21 @@ fn generate_content_id(content: &str)-> [u8; 32]{
     content_id
 }
 
+// 鍵の生成
+fn generate_key() -> (SigningKey, VerifyingKey) {
+    let secret_key = SigningKey::random(&mut OsRng);
+    let public_key = VerifyingKey::from(&secret_key);
+    (secret_key, public_key)
+}
+
 fn main(){
     // 事前準備
-    let public_key_a = "public_key_A";
-    let public_key_b = "public_key_B";
+    let (_, public_key_a) = generate_key();
+    let (_, public_key_b) = generate_key();
     let content_data = "test";
     // Peeridの生成
-    let peer_id_a = generate_peer_id(&public_key_a);
-    let peer_id_b = generate_peer_id(&public_key_b);
+    let peer_id_a = generate_peer_id(&public_key_a.to_encoded_point(false).as_bytes());
+    let peer_id_b = generate_peer_id(&public_key_b.to_encoded_point(false).as_bytes());
 
     let content_id = generate_content_id(&content_data);
 
